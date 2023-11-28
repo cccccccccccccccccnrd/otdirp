81f0ffc0546d8deffc6fac479b238d5a00fc59be
cccccccccccccccccnrd <hello@conradweise.com>
2023-11-28 19:49:57 +0100

    no leaks pls

--- src/auto/index.js
+++ src/auto/index.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer')
 const path = require('path')
 const fs = require('fs')
+let browser
 
 const delay = milliseconds =>
   new Promise(resolve => setTimeout(resolve, milliseconds))
@@ -16,9 +17,9 @@ async function save (base64, url, count, timestamp) {
 }
 
 async function go (url) {
+  const page = await browser.newPage()
+
   try {
-    const browser = await puppeteer.launch({ headless: 'new' })
-    const page = await browser.newPage()
     page.setDefaultNavigationTimeout(10000)
     const now = Date.now()
     let count = 0
@@ -40,16 +41,22 @@ async function go (url) {
 
     await page.goto(`http://${url}`, { waitUntil: 'networkidle2' })
     await delay(2000)
-    await page.close()
 
     return true
   } catch (error) {
     console.log(error)
+  } finally {
+    await page.close()
   }
 }
 
 async function init () {
-  const urls = fs.readFileSync(path.join(__dirname, './500.txt'), 'utf-8').replace(/^[#;].*$/gm, '').replace(/(\r?\n)(?:\r?\n)+/gm, '').replace(/0.0.0.0 /gm, '').split('\n')
+  const urls = fs
+    .readFileSync(path.join(__dirname, './500.txt'), 'utf-8')
+    .replace(/^[#;].*$/gm, '')
+    .replace(/(\r?\n)(?:\r?\n)+/gm, '')
+    .replace(/0.0.0.0 /gm, '')
+    .split('\n')
   console.log(urls.length)
 
   /* const urls = [
@@ -58,8 +65,8 @@ async function init () {
     'http://tiktok.com'
   ] */
 
+  browser = await puppeteer.launch({ headless: 'new' })
   let count = 0
-
   for (const url of urls) {
     console.log(count, url)
     await go(url)
