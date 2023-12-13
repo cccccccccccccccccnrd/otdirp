b35cb09d4721b8aa7dce262ffe6736248fa48342
cccccccccccccccccnrd <hello@conradweise.com>
2023-12-13 19:29:03 +0100

    screens

---
+++ src/screens/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+
+const app = express()
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.get('/context', (req, res) => {
+  const files = fs
+    .readdirSync(path.join(__dirname, 'public/context'))
+    .filter(e => e.includes('.'))
+  res.send(files)
+})
+
+app.get('/archive', (req, res) => {
+  const files = fs
+    .readdirSync(path.join(__dirname, 'public/archive'))
+    .filter(e => e.includes('.'))
+  res.send(files)
+})
+
+app.listen(2222)
+console.log(
+  'On the Difference in Rendering Pixels listening on http://localhost:2222'
+)
---
+++ src/screens/public/screens.js
@@ -0,0 +1,34 @@
+const mode = new URLSearchParams(window.location.search).get('mode')
+
+console.log(mode)
+if (mode) init()
+
+async function f (url) {
+  const response = await fetch(url)
+  return await response.json()
+}
+
+async function init () {
+  const m = await f(`/${mode}`)
+  let count = 0
+
+  setInterval(() => {
+    i.src = `/${mode}/${m[count]}`
+
+    switch (mode) {
+      case 'archive':
+        i.style.imageRendering = 'pixelated'
+        break
+      case 'context':
+        if (m[count].split('.')[0].endsWith('-i')) {
+          i.style.filter = 'invert(1)'
+        } else {
+          i.style.filter = ''
+        }
+        break
+    }
+
+    count === m.length - 1 ? (count = 0) : count++
+    console.log(count, m[count])
+  }, 1000)
+}
